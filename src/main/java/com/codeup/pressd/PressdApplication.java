324c692ffd08f61cda781a648dad4fd0ee1697c9
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-04-13 10:52:31 -0400

    Initialized Repository

---
+++ src/main/java/com/codeup/pressd/PressdApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.pressd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PressdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PressdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeup/pressd/PressdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.pressd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PressdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
