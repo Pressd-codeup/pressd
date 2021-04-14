09869235f4858b908b2003ad08a6a2a6f885c010
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-04-14 11:08:28 -0500

    template reorganization

---
+++ src/main/java/com/codeup/pressd/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.codeup.pressd.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String welcome(){
+        return "web-pages/Landing/landing";
+    }
+}
