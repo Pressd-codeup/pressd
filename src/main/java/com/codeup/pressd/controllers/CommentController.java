459c64c7be194644689df19807f877b655b68fa2
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2021-04-15 13:47:41 -0500

    PostMapping for create comment

--- src/main/java/com/codeup/pressd/controllers/CommentController.java
+++ src/main/java/com/codeup/pressd/controllers/CommentController.java
@@ -2,6 +2,7 @@ package com.codeup.pressd.controllers;
 
 import com.codeup.pressd.models.*;
 import com.codeup.pressd.repository.*;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -20,5 +21,19 @@ public class CommentController {
 		this.commentDao = commentDao;
 		this.userDao = userDao;
 		this.workoutDao = workoutDao;
+	}
+
+	@PostMapping("/comment/create")
+	public String createComment(@RequestParam(name = "body") String body,
+								@RequestParam(name = "workout_id") long workout_id){
+		User userToAdd = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Comment newComment = new Comment();
+		Workout workout = workoutDao.getOne(workout_id);
+		newComment.setBody(body);
+		newComment.setUser(userToAdd);
+		newComment.setWorkout(workout);
+		workoutDao.save(workout);
+
+		return "workouts/index";
 	}
 }
