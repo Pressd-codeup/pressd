a0b367c57c43947c5164ca98043c6f22f478b57a
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-04-15 11:12:27 -0400

    workout controller/repo

--- src/main/java/com/codeup/pressd/controllers/WorkoutController.java
+++ src/main/java/com/codeup/pressd/controllers/WorkoutController.java
@@ -1,8 +1,47 @@
 package com.codeup.pressd.controllers;
 
 
+import com.codeup.pressd.models.Category;
+import com.codeup.pressd.models.Workout;
+import com.codeup.pressd.repository.CategoryRepository;
+import com.codeup.pressd.repository.UserRepository;
+import com.codeup.pressd.repository.WorkoutRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class WorkoutController {
+
+	private final WorkoutRepository workoutDao;
+	private final UserRepository userDao;
+	private final CategoryRepository categoryDao;
+
+	WorkoutController(WorkoutRepository workoutDao, UserRepository userDao, CategoryRepository categoryDao) {
+		this.workoutDao = workoutDao;
+		this.userDao = userDao;
+		this.categoryDao = categoryDao;
+
+	}
+
+	@GetMapping("/workouts")
+	public String seeAllWorkouts(Model viewModel) {
+		List<Workout> workouts = workoutDao.findAll();
+		viewModel.addAttribute("workouts", workouts);
+		return "workouts/index";
+	}
+
+	@GetMapping("/workouts/category/{id}")
+	public String viewWorkoutsByCategory(@PathVariable long id, Model viewModel) {
+
+		Category category = categoryDao.getOne(id);
+		List<Workout> workouts = workoutDao.getWorkoutsByCategoriesContaining(category);
+		viewModel.addAttribute("workouts", workouts);
+		return "workouts/categories";
+	}
+
+
 }
--- src/main/java/com/codeup/pressd/repository/WorkoutRepository.java
+++ src/main/java/com/codeup/pressd/repository/WorkoutRepository.java
@@ -1,9 +1,14 @@
 package com.codeup.pressd.repository;
 
 import com.codeup.pressd.models.Workout;
+import com.codeup.pressd.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WorkoutRepository extends JpaRepository<Workout, Long> {
+
+	List<Workout> getWorkoutsByCategoriesContaining(Category category);
 }
