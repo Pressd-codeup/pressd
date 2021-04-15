ce93959eee533f6611f45d99fcaeef81a0b9287b
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2021-04-15 09:56:58 -0500

    Posts/create get mapping + added maven security dependancy

--- src/main/java/com/codeup/pressd/controllers/PostController.java
+++ src/main/java/com/codeup/pressd/controllers/PostController.java
@@ -1,13 +1,18 @@
 package com.codeup.pressd.controllers;
 
 import com.codeup.pressd.models.Post;
+import com.codeup.pressd.models.Type;
 import com.codeup.pressd.models.User;
 import com.codeup.pressd.repository.PostRepository;
 import com.codeup.pressd.repository.UserRepository;
+import com.codeup.pressd.repository.TypeRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 import java.util.List;
 
@@ -16,10 +21,12 @@ public class PostController {
 
     private final PostRepository postDao;
     private final UserRepository userDao;
+    private final TypeRepository typeDao;
 
-    PostController(PostRepository postDao, UserRepository userDao){
+    PostController(PostRepository postDao, UserRepository userDao, TypeRepository typeDao){
         this.postDao = postDao;
         this.userDao = userDao;
+        this.typeDao = typeDao;
     }
 
     @GetMapping("/partners")
@@ -53,5 +60,58 @@ public class PostController {
         viewModel.addAttribute("post", post);
         viewModel.addAttribute("user", user);
         return "posts/show";
+    }
+
+    @GetMapping("/posts/{id}")
+    public String showOnePost(@PathVariable Long id, Model vModel){
+        vModel.addAttribute("post", postDao.getOne(id));
+        return "posts/show";
+    }
+
+    @GetMapping("/posts/create")
+    public String viewPostForm(Model vModel){
+        vModel.addAttribute("post",new Post());
+        return "posts/create";
+    }
+
+    @PostMapping("/posts/create")
+    public String createPost(@RequestParam(name = "body") String body,
+                             @RequestParam(name = "title") String title,
+                             @RequestParam(name = "zipcode") int zipcode,
+                             @RequestParam(name = "type_id") int type_id){
+
+        User userToAdd = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Post newPost = new Post();
+        Type type = new Type();
+
+        type.setId(type_id);
+        type.setName(typeDao.getName(type_id));
+
+        newPost.setUser(userToAdd);
+        newPost.setBody(body);
+        newPost.setTitle(title);
+        newPost.setZipcode(zipcode);
+        newPost.setType(type);
+        postDao.save(newPost);
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/posts/{id}/update")
+    public String viewEditForm(Model vModel, @PathVariable Long id){
+        vModel.addAttribute("post",postDao.getOne(id));
+        return "posts/update";
+    }
+
+    @PostMapping("/posts/{id}/update")
+    public String editPost(@ModelAttribute Post postToUpdate, @PathVariable Long id){
+
+        User userToAdd = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        System.out.println();
+        postToUpdate.setId(id);
+        postToUpdate.setUser(userToAdd);
+        postDao.save(postToUpdate);
+
+        return "redirect:/posts";
     }
 }
