d8dc7b94d0e949a521c1b2d410ff7fd0d177381b
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-04-15 10:26:21 -0400

    Repos

--- src/main/java/com/codeup/pressd/repository/PostRepository.java
+++ src/main/java/com/codeup/pressd/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.codeup.pressd.repository;
 
 import com.codeup.pressd.models.Post;
+import com.codeup.pressd.models.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 
+	List<Post> getPostsByTypeName(String type_name);
 }
