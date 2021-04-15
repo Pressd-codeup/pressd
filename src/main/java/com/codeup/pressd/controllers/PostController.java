747244fedc869f7a579cd46f7cbf084bb1756db8
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2021-04-15 09:58:44 -0500

    reindented file with tab

--- src/main/java/com/codeup/pressd/controllers/PostController.java
+++ src/main/java/com/codeup/pressd/controllers/PostController.java
@@ -19,99 +19,99 @@ import java.util.List;
 @Controller
 public class PostController {
 
-    private final PostRepository postDao;
-    private final UserRepository userDao;
-    private final TypeRepository typeDao;
+	private final PostRepository postDao;
+	private final UserRepository userDao;
+	private final TypeRepository typeDao;
 
-    PostController(PostRepository postDao, UserRepository userDao, TypeRepository typeDao){
-        this.postDao = postDao;
-        this.userDao = userDao;
-        this.typeDao = typeDao;
-    }
+	PostController(PostRepository postDao, UserRepository userDao, TypeRepository typeDao){
+		this.postDao = postDao;
+		this.userDao = userDao;
+		this.typeDao = typeDao;
+	}
 
-    @GetMapping("/partners")
-    public String seeBuddyPosts(Model viewModel){
+	@GetMapping("/partners")
+	public String seeBuddyPosts(Model viewModel){
 
-        List<Post> posts = postDao.getPostsByTypeName("partners");
-        viewModel.addAttribute("posts", posts);
-        return "posts/index";
-    }
+		List<Post> posts = postDao.getPostsByTypeName("partners");
+		viewModel.addAttribute("posts", posts);
+		return "posts/index";
+	}
 
-    @GetMapping("/coaches")
-    public String seeCoachPosts(Model viewModel) {
-        List<Post> posts = postDao.getPostsByTypeName("coaches");
-        viewModel.addAttribute("posts", posts);
-        return "posts/index";
-    }
+	@GetMapping("/coaches")
+	public String seeCoachPosts(Model viewModel) {
+		List<Post> posts = postDao.getPostsByTypeName("coaches");
+		viewModel.addAttribute("posts", posts);
+		return "posts/index";
+	}
 
-    @GetMapping("/clients")
-    public String seeClientPosts(Model viewModel) {
-        List<Post> posts = postDao.getPostsByTypeName("clients");
-        viewModel.addAttribute("posts", posts);
-        return "posts/index";
-    }
+	@GetMapping("/clients")
+	public String seeClientPosts(Model viewModel) {
+		List<Post> posts = postDao.getPostsByTypeName("clients");
+		viewModel.addAttribute("posts", posts);
+		return "posts/index";
+	}
 
 
 
-    @GetMapping("/posts/{id}")
-    public String showOnePost(@PathVariable long id, Model viewModel){
-        Post post = postDao.getOne(id);
-        User user = post.getUser();
-        viewModel.addAttribute("post", post);
-        viewModel.addAttribute("user", user);
-        return "posts/show";
-    }
+	@GetMapping("/posts/{id}")
+	public String showOnePost(@PathVariable long id, Model viewModel){
+		Post post = postDao.getOne(id);
+		User user = post.getUser();
+		viewModel.addAttribute("post", post);
+		viewModel.addAttribute("user", user);
+		return "posts/show";
+	}
 
-    @GetMapping("/posts/{id}")
-    public String showOnePost(@PathVariable Long id, Model vModel){
-        vModel.addAttribute("post", postDao.getOne(id));
-        return "posts/show";
-    }
+	@GetMapping("/posts/{id}")
+	public String showOnePost(@PathVariable Long id, Model vModel){
+		vModel.addAttribute("post", postDao.getOne(id));
+		return "posts/show";
+	}
 
-    @GetMapping("/posts/create")
-    public String viewPostForm(Model vModel){
-        vModel.addAttribute("post",new Post());
-        return "posts/create";
-    }
+	@GetMapping("/posts/create")
+	public String viewPostForm(Model vModel){
+		vModel.addAttribute("post",new Post());
+		return "posts/create";
+	}
 
-    @PostMapping("/posts/create")
-    public String createPost(@RequestParam(name = "body") String body,
-                             @RequestParam(name = "title") String title,
-                             @RequestParam(name = "zipcode") int zipcode,
-                             @RequestParam(name = "type_id") int type_id){
+	@PostMapping("/posts/create")
+	public String createPost(@RequestParam(name = "body") String body,
+							 @RequestParam(name = "title") String title,
+							 @RequestParam(name = "zipcode") int zipcode,
+							 @RequestParam(name = "type_id") int type_id){
 
-        User userToAdd = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Post newPost = new Post();
-        Type type = new Type();
+		User userToAdd = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Post newPost = new Post();
+		Type type = new Type();
 
-        type.setId(type_id);
-        type.setName(typeDao.getName(type_id));
+		type.setId(type_id);
+		type.setName(typeDao.getName(type_id));
 
-        newPost.setUser(userToAdd);
-        newPost.setBody(body);
-        newPost.setTitle(title);
-        newPost.setZipcode(zipcode);
-        newPost.setType(type);
-        postDao.save(newPost);
-        return "redirect:/posts";
-    }
+		newPost.setUser(userToAdd);
+		newPost.setBody(body);
+		newPost.setTitle(title);
+		newPost.setZipcode(zipcode);
+		newPost.setType(type);
+		postDao.save(newPost);
+		return "redirect:/posts";
+	}
 
-    @GetMapping("/posts/{id}/update")
-    public String viewEditForm(Model vModel, @PathVariable Long id){
-        vModel.addAttribute("post",postDao.getOne(id));
-        return "posts/update";
-    }
+	@GetMapping("/posts/{id}/update")
+	public String viewEditForm(Model vModel, @PathVariable Long id){
+		vModel.addAttribute("post",postDao.getOne(id));
+		return "posts/update";
+	}
 
-    @PostMapping("/posts/{id}/update")
-    public String editPost(@ModelAttribute Post postToUpdate, @PathVariable Long id){
+	@PostMapping("/posts/{id}/update")
+	public String editPost(@ModelAttribute Post postToUpdate, @PathVariable Long id){
 
-        User userToAdd = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		User userToAdd = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        System.out.println();
-        postToUpdate.setId(id);
-        postToUpdate.setUser(userToAdd);
-        postDao.save(postToUpdate);
+		System.out.println();
+		postToUpdate.setId(id);
+		postToUpdate.setUser(userToAdd);
+		postDao.save(postToUpdate);
 
-        return "redirect:/posts";
-    }
+		return "redirect:/posts";
+	}
 }
