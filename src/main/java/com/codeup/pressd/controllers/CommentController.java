94124d4fb946f69a3b921b9c341b2c89eb15b1ed
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-04-20 11:57:01 -0500

    feat: comment delete

--- src/main/java/com/codeup/pressd/controllers/CommentController.java
+++ src/main/java/com/codeup/pressd/controllers/CommentController.java
@@ -3,49 +3,63 @@ package com.codeup.pressd.controllers;
 
 import com.codeup.pressd.models.*;
 import com.codeup.pressd.repository.*;
+import org.dom4j.rule.Mode;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
 @Controller
 public class CommentController {
 
-	private final CommentRepository commentDao;
-	private final UserRepository userDao;
-	private final WorkoutRepository workoutDao;
+    private final CommentRepository commentDao;
+    private final UserRepository userDao;
+    private final WorkoutRepository workoutDao;
 
-	CommentController(CommentRepository commentDao, UserRepository userDao, WorkoutRepository workoutDao){
-		this.commentDao = commentDao;
-		this.userDao = userDao;
-		this.workoutDao = workoutDao;
-	}
+    CommentController(CommentRepository commentDao, UserRepository userDao, WorkoutRepository workoutDao) {
+        this.commentDao = commentDao;
+        this.userDao = userDao;
+        this.workoutDao = workoutDao;
+    }
 
-	@PostMapping("/comments/create")
-	public String createComment(@ModelAttribute Comment comment){
-		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		comment.setUser(user);
-		commentDao.save(comment);
-		return "redirect:/workouts/show";
-	}
+    @GetMapping("/workouts/{id}/comments/create")
+    public String showCreateComment(@PathVariable long id, Model viewModel) {
+        viewModel.addAttribute("comment", new Comment());
+        return "comments/create";
+    }
 
-	@PostMapping("/comments/{id}/update")
-	public String editComment(@ModelAttribute Comment commentToUpdate, @PathVariable long id) {
-		//WILL NEED AUTHENTICATION OF CURRENTUSER == POSTUSER
-		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		commentDao.save(commentToUpdate);
-		return "redirect:/posts";
-	}
+    @PostMapping("/workouts/{id}/comments/create")
+    public String createComment(@PathVariable long id, @ModelAttribute Comment comment) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Workout workout = workoutDao.getOne(id);
+        comment.setUser(user);
+        comment.setDatePosted(LocalDateTime.now());
+        comment.setWorkout(workout);
+        commentDao.save(comment);
+        return "redirect:/workouts/show";
+    }
 
-	@PostMapping("/comments/{id}/delete")
-	public String deleteComment(@ModelAttribute Comment commentToDelete, @PathVariable long id){
+    @PostMapping("/comments/{id}/update")
+    public String editComment(@ModelAttribute Comment commentToUpdate, @PathVariable long id) {
+        //WILL NEED AUTHENTICATION OF CURRENTUSER == POSTUSER
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        commentDao.save(commentToUpdate);
+        return "redirect:/posts";
+    }
 
-		//WILL NEED AUTHENTICATION OF CURRENTUSER == POSTUSER
-
-		commentDao.delete(commentToDelete);
-		return "redirect:/workouts/index";
-	}
+    @PostMapping("/comments/{id}/delete")
+    public String deleteComment(@PathVariable long id) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        //WILL NEED AUTHENTICATION OF CURRENTUSER == POSTUSER
+        Comment comment = commentDao.getOne(id);
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (currentUser.getId() == comment.getUser().getId()) {
+            commentDao.deleteById(id);
+        }
+        return "redirect:/workouts/index";
+    }
 }
--- src/main/java/com/codeup/pressd/controllers/WorkoutController.java
+++ src/main/java/com/codeup/pressd/controllers/WorkoutController.java
@@ -104,4 +104,5 @@ public class WorkoutController {
 		return "redirect:/workouts";
 	}
 
+
 }
