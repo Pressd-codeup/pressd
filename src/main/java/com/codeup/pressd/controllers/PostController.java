fb5939345fab9974095846e3878d665ead8d9fc1
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2021-04-22 07:18:56 -0500

    filter functionality complete

--- src/main/java/com/codeup/pressd/controllers/PostController.java
+++ src/main/java/com/codeup/pressd/controllers/PostController.java
@@ -12,10 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 
 @Controller
@@ -59,138 +56,28 @@ public class PostController {
 	}
 	@GetMapping("/posts/filter")
 	public String filterPosts(Model viewModel){
-		viewModel.addAttribute("post", new Post());
+		viewModel.addAttribute("filter", new Filter());
 		return "posts/filter";
 	}
 
 	@PostMapping("/posts/filter")
 	public String showFilteredPosts(@ModelAttribute Filter filter, Model viewModel) {
-
-
+		Post placeHolder = new Post();
 		List<Post> posts = postDao.getPostsByTypeName(filter.getType_name());
-		List<Post> filteredPosts = new List<Post>() {
-			@Override
-			public int size() {
-				return 0;
-			}
-
-			@Override
-			public boolean isEmpty() {
-				return false;
-			}
-
-			@Override
-			public boolean contains(Object o) {
-				return false;
-			}
-
-			@Override
-			public Iterator<Post> iterator() {
-				return null;
-			}
-
-			@Override
-			public Object[] toArray() {
-				return new Object[0];
-			}
-
-			@Override
-			public <T> T[] toArray(T[] a) {
-				return null;
-			}
-
-			@Override
-			public boolean add(Post post) {
-				return false;
-			}
-
-			@Override
-			public boolean remove(Object o) {
-				return false;
-			}
-
-			@Override
-			public boolean containsAll(Collection<?> c) {
-				return false;
-			}
-
-			@Override
-			public boolean addAll(Collection<? extends Post> c) {
-				return false;
-			}
-
-			@Override
-			public boolean addAll(int index, Collection<? extends Post> c) {
-				return false;
-			}
-
-			@Override
-			public boolean removeAll(Collection<?> c) {
-				return false;
-			}
-
-			@Override
-			public boolean retainAll(Collection<?> c) {
-				return false;
-			}
-
-			@Override
-			public void clear() {
-
-			}
-
-			@Override
-			public Post get(int index) {
-				return null;
-			}
-
-			@Override
-			public Post set(int index, Post element) {
-				return null;
-			}
-
-			@Override
-			public void add(int index, Post element) {
-
-			}
-
-			@Override
-			public Post remove(int index) {
-				return null;
-			}
-
-			@Override
-			public int indexOf(Object o) {
-				return 0;
-			}
-
-			@Override
-			public int lastIndexOf(Object o) {
-				return 0;
-			}
-
-			@Override
-			public ListIterator<Post> listIterator() {
-				return null;
-			}
-
-			@Override
-			public ListIterator<Post> listIterator(int index) {
-				return null;
-			}
-
-			@Override
-			public List<Post> subList(int fromIndex, int toIndex) {
-				return null;
-			}
-		};
+		ArrayList<Post> filteredPosts = new ArrayList<>();
+		filteredPosts.add(0,placeHolder);
 		for (Post post : posts) {
 			if (filter.getParams().contains(String.valueOf(post.getZipcode()))) {
 				filteredPosts.add(post);
 			}
 		}
-		viewModel.addAttribute("posts", filteredPosts);
-		return "posts/index";
+		if(filteredPosts.size() == 1){
+			return "redirect:/posts";
+		} else {
+			filteredPosts.remove(0);
+			viewModel.addAttribute("posts", filteredPosts);
+		}
+		return "/posts/index";
 	}
 
 	@GetMapping("/posts/{id}")
